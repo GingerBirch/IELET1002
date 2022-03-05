623e01bcb3342eb8e3ffcd9f6d5d5e5b1e6de603
TheBirch01 <44576321+YoFavBirch@users.noreply.github.com>
2022-03-05 21:52:32 +0100

    Update Oppg_4.ino

--- Lab 4/Oppg_4/Oppg_4.ino
+++ Lab 4/Oppg_4/Oppg_4.ino
@@ -1,36 +1,37 @@
-#include <Zumo32U4.h>
+#include <Zumo32U4.h> // inkluderer Zumo-biblioteket
 
-Zumo32U4ButtonA buttonA;
-Zumo32U4LCD display;
-Zumo32U4Motors motors;
-Zumo32U4LineSensors lineSensors;
+Zumo32U4ButtonA buttonA;         // deklarerer knapp A
+Zumo32U4LCD display;             // deklarerer LCD-skjerm
+Zumo32U4Motors motors;           // deklarerer motorene
+Zumo32U4LineSensors lineSensors; // deklarerer linjesensorene
 
-#define NUM_SENSORS 5
-unsigned int lineSensorValues[NUM_SENSORS];
-int16_t lastError = 0;
-const uint16_t maxSpeed = 400;
+#define NUM_SENSORS 5                       // definerer hvor mange sensorer det er
+unsigned int lineSensorValues[NUM_SENSORS]; // lager liste med lengde likt antall sensorer
+int16_t lastError = 0;                      // deklarerer siste avstand fra teip
+const uint16_t maxSpeed = 400;              // deklarerer maks fart
 
+// funksjon som kalibrerer linjesensorene
 void sensorsCalibrate(){
-  display.clear();
-  delay(1000);
-  unsigned long calibrateTime = millis() + 2000;
+  display.clear(); // fjerner alt fra LCD-skjerm
+  delay(1000); // venter 1 sekund for å få fjernet fingeren fra knappen
+  unsigned long calibrateTime = millis() + 2000; // lagrer hvor lenge Zumo skal kalibreres
   while (millis() < calibrateTime){
-    motors.setSpeeds(200, -200);
-    lineSensors.calibrate();
+    motors.setRightSpeed(maxSpeed); // setter høyrehjulet til maks fart
+    lineSensors.calibrate(); // kalibrerer linjesensorene
   }
-  motors.setRightSpeed(0);
+  motors.setRightSpeed(0); // stopper Zumo
 }
 
 void setup() {
-  lineSensors.initFiveSensors();
+  lineSensors.initFiveSensors(); // konfigurerer Zumo til å bruke alle fem sensorer
   display.clear();
-  display.print(F("Press A"));
-  display.gotoXY(0, 1);
+  display.print(F("Press A")); // printer melding til LCD-skjerm
+  display.gotoXY(0, 1); // flytter markøren en linje ned
   display.print(F("to calib"));
-  buttonA.waitForButton();
+  buttonA.waitForButton(); // setter programmet på pause til knappen trykkes
   
-  sensorsCalibrate();
-  lineSensors.readCalibrated(lineSensorValues);
+  sensorsCalibrate(); // kjører kalibrering
+  lineSensors.readCalibrated(lineSensorValues); // leser de kalibrerte verdiene til sensorene
 
   display.print(F("Press A"));
   display.gotoXY(0, 1);
@@ -39,22 +40,27 @@ void setup() {
   display.clear();
 }
 
+// funksjon som setter motorene til en hviss fart
 void setMotors(){
-  int16_t position = lineSensors.readLine(lineSensorValues);
-  int16_t error = position - 2000;
-  int16_t speedDifference = error / 4 + 6 * (error - lastError);
+  int16_t position = lineSensors.readLine(lineSensorValues); // tar inn posisjonen i forhild til teipen
+  int16_t error = position - 2000; // lagrer hvor langt Zumo er fra teipen
+  int16_t speedDifference = error / 4 + 6 * (error - lastError); // regner ut hastighetsforskjellen til hjulene
 
-  lastError = error;
+  lastError = error; // lagrer forrige avstand fra teipen
 
+  // legger til maksfart +- hastighetsforskjellen
   int16_t leftSpeed = (int16_t)maxSpeed + speedDifference;
   int16_t rightSpeed = (int16_t)maxSpeed - speedDifference;
 
+  // hindrer farten i å overstige maks fart
   leftSpeed = constrain(leftSpeed, 0, (int16_t)maxSpeed);
   rightSpeed = constrain(rightSpeed, 0, (int16_t)maxSpeed);
 
+  // setter hastighetene til hjulene
   motors.setSpeeds(leftSpeed, rightSpeed);
 }
 
+// kjører motor-funksjonen hele tiden
 void loop() {
   setMotors();
 }
